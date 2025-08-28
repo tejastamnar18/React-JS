fd60fb7aa72de7ce9b4793eaa84144947084ccea
tejastamnar18 <tejastamnar8034@gmail.com>
2025-08-28 07:51:28 +0000

    Basic React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello World || First Code in react</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Hello world With vite | Tejas</h1>
+  )
+}
+
+export default App
